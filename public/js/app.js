cdd88f472bed0090becb1298a40720a9c84fe2cd
Daniil Grodskiy <daniilgrodskiy@Daniils-MacBook-Pro.local>
2019-10-06 14:23:33 -0400

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,10 @@
 // Code that's gonna run on the browser
 // Gonna be using fetch API - which is just used only on client-side js NOT server-side
 
-console.log('Client side javascript file is loaded')
-
-fetch("http://localhost:3000/weather?address=!").then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
